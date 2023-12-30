801b7ec4c16eea6926a27c23dcee50ca0d3391a6
fatih yavuz <fatihyvz993@gmail.com>
2023-12-30 20:49:16 +0300

    v1

--- js/practice-02.js
+++ js/practice-02.js
@@ -1,11 +1,2 @@
-// ornek1 diktortgenin alanini hesaplayan bir kod yaziniz 
-
-let uzunkenar = prompt("lutfen uzun kenari giriniz")
-
-let kisakenar = prompt("lutfen kisa kenari giriniz")
-
-alan = uzunkenar*kisakenar
-
-cevre = uzunkenar *2 + kisakenar*2 
-
-console.log('alan:', alan , "cevre:", cevre)
+let pisayisi = 3.14
+console.log(typeof(pisayisi))
